27f5ea637a58f5b7c00d55a6761c985fe40f002a
qxxxb <46016483+qxxxb@users.noreply.github.com>
2020-10-29 13:12:47 -0400

    Add Q5.a
    - Print names of queries
    - Using pretty print

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sqlite3
+import pprint
 import os
+
+pp = pprint.PrettyPrinter(indent=4)
 
 db_name = 'a.db'
 
@@ -13,15 +16,29 @@ c = conn.cursor()
 
 # Q2
 
+print('# Question 2')
+print()
+
 with open('create.sql', 'r') as f:
     s = f.read()
     c.executescript(s)
+    print('Created database')
 
 with open('init.sql', 'r') as f:
     s = f.read()
     c.executescript(s)
+    print('Initialized database with sample data')
+
 
 # Q3
+print()
+print('# Question 3')
+print()
+
+print(
+    '(3.a) Find the titles of all items by a given seller that cast less '
+    'than $10'
+)
 
 seller = "johndoe@gmail.com"
 s = """
@@ -35,7 +52,12 @@ WHERE
 """
 
 c.execute(s, (seller,))
-print(c.fetchall())
+pp.pprint(c.fetchall())
+print()
+
+print(
+    '(3.b) Give all titles and their dates of purchase made by a given buyer'
+)
 
 buyer = "janedoe@gmail.com"
 s = """
@@ -48,7 +70,12 @@ WHERE
 """
 
 c.execute(s, (buyer,))
-print(c.fetchall())
+pp.pprint(c.fetchall())
+print()
+
+print(
+    '(3.c) Find seller names with less than 5 items for sale'
+)
 
 s = """
 SELECT user.first_name, user.last_name
@@ -61,7 +88,12 @@ HAVING COUNT(item.serial_no) < 5;
 """
 
 c.execute(s)
-print(c.fetchall())
+print()
+
+print(
+    '(3.d) Give all buyers who purchased an item by a given seller and the'
+    'names of the items they purchased'
+)
 
 seller = "johndoe@gmail.com"
 s = """
@@ -76,7 +108,11 @@ WHERE
 """
 
 c.execute(s, (seller,))
-print(c.fetchall())
+print()
+
+print(
+    '(3.e) Find the total number of items purchased by a given buyer'
+)
 
 buyer = "janedoe@gmail.com"
 s = """
@@ -89,7 +125,12 @@ WHERE
 """
 
 c.execute(s, (buyer,))
-print(c.fetchall())
+pp.pprint(c.fetchall())
+print()
+
+print(
+    '(3.f) Find the buyer who purchased the most number of items'
+)
 
 s = """
 SELECT email, MAX(`COUNT(item.serial_no)`)
@@ -104,4 +145,29 @@ FROM (
 """
 
 c.execute(s)
-print(c.fetchall())
+pp.pprint(c.fetchall())
+
+# TODO: Q4
+
+# Q5
+print()
+print('# Question 5')
+print()
+
+print('(5.a) List of buyer names with total dollar amount spent')
+
+s = """
+SELECT
+    user.first_name,
+    user.last_name,
+    ROUND(SUM(item.price / 100.0), 2) dollars_spent
+FROM user, buyer, buyer_order, item
+WHERE
+    user.email = buyer.email AND
+    buyer.email = buyer_order.buyer_email AND
+    buyer_order.id = item.buyer_order_id
+GROUP BY user.email;
+"""
+
+c.execute(s)
+pp.pprint(c.fetchall())
